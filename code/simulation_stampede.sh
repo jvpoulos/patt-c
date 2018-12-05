9dbf4a76f04323708322f35622088336237d364d
Jason Poulos <jvpoulos@gmail.com>
2018-12-05 10:27:48 -0800

    stampede

---
+++ code/simulation_stampede.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#----------------------------------------------------
+#SBATCH -J simulation_stampede           # Job name
+#SBATCH -o simulation_stampede.o%j       # Name of stdout output file
+#SBATCH -e simulation_stampede.e%j       # Name of stderr error file
+#SBATCH -p normal		        # Queue (partition) name
+#SBATCH -N 1              		# Total # of nodes (must be 1 for serial)
+#SBATCH -n 1              	 	# Total # of mpi tasks (should be 1 for serial)
+#SBATCH -t 24:00:00        		# Run time (hh:mm:ss)
+#SBATCH --mail-user=poulos@berkeley.edu
+#SBATCH --mail-type=all    		# Send email at begin and end of job
+
+R --no-save < simulation.R
